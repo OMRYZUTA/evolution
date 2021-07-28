06cfeb9979cfd4ece641e9108b1c4be16b4e31a0
Orna Lipkin <fifilp@gmail.com>
2021-07-28 11:07:39 +0300

    changes to setDescriptor

--- engine/src/il/ac/mta/zuli/evolution/engine/Engine.java
+++ engine/src/il/ac/mta/zuli/evolution/engine/Engine.java
@@ -1,7 +1,7 @@
 package il.ac.mta.zuli.evolution.engine;
 
 public interface Engine {
-    void loadXML(String path);
+    void loadXML(String path); //in impl of method - use other method to check if a valid file is already loaded
 
     //shows details re the algorithm and schedule received via file
     void showDetails();
--- engine/src/il/ac/mta/zuli/evolution/engine/data/Descriptor.java
+++ engine/src/il/ac/mta/zuli/evolution/engine/data/Descriptor.java
@@ -6,10 +6,13 @@ import il.ac.mta.zuli.evolution.engine.data.generated.ETTDescriptor;
 //order of ctors - for example, subjects to be created before teachers
 
 public class Descriptor {
-    private TimeTable timeTable = null;
-    private EngineSettings engine = null;
+    private TimeTable timeTable;
+    private EngineSettings engine;
     /* pre-initialized instance of the singleton */
-    private static Descriptor instance = null;
+    private static Descriptor instance;
+
+    private Descriptor() {
+    }
 
     /* Access point to the unique instance of the singleton */
     public static Descriptor getInstance() {
@@ -23,26 +26,20 @@ public class Descriptor {
     }
 
     public void setDescriptor(ETTDescriptor d) {
-        if ((timeTable == null) && (engine == null)) {
-            try {
-                timeTable = new TimeTable(d.getETTTimeTable());
-                engine = new EngineSettings(d.getETTEvolutionEngine());
-                System.out.println("in setDescriptor" + timeTable + engine); //delete later
-            } catch (Exception e) {
-                //TODO catch exception
-            }
-        } else {
-            //only if received another valid file we want to overwrite the previous descriptor instance
-            TimeTable tempTimeTable = null;
-            EngineSettings tempEngineSetting = null;
-            try {
-                tempTimeTable = new TimeTable(d.getETTTimeTable());
-                tempEngineSetting = new EngineSettings(d.getETTEvolutionEngine());
-            } catch (Exception e) {
-                //change later to the exception relevant to me - delete later
-            }
+        //only if received another valid file we want to overwrite the previous descriptor instance
+        TimeTable tempTimeTable = null;
+        EngineSettings tempEngineSetting = null;
+
+        try {
+            tempTimeTable = new TimeTable(d.getETTTimeTable());
+            tempEngineSetting = new EngineSettings(d.getETTEvolutionEngine());
+
             timeTable = tempTimeTable;
             engine = tempEngineSetting;
+            System.out.println("in setDescriptor" + timeTable + engine); //delete later
+        } catch (Exception e) {
+            //did we previously have a valid file loaded or was Descriptor empty - loadXML method will handle this
+            //TODO throw exception
         }
     }
 
