55122cd341ce94d7f7e38da3beae5477011ebd79
OMRYZUTA <omrizu@mta.ac.il>
2021-08-08 12:49:30 +0300

    added exception in jaxb

--- engine/src/il/ac/mta/zuli/evolution/engine/xmlparser/XMLParser.java
+++ engine/src/il/ac/mta/zuli/evolution/engine/xmlparser/XMLParser.java
@@ -1,6 +1,8 @@
 package il.ac.mta.zuli.evolution.engine.xmlparser;
 
 import il.ac.mta.zuli.evolution.engine.Descriptor;
+import il.ac.mta.zuli.evolution.engine.exceptions.EmptyCollectionException;
+import il.ac.mta.zuli.evolution.engine.exceptions.ValidationException;
 import il.ac.mta.zuli.evolution.engine.xmlparser.generated.ETTDescriptor;
 import org.jetbrains.annotations.NotNull;
 
@@ -10,13 +12,22 @@ import java.io.File;
 
 public class XMLParser {
     public Descriptor unmarshall(@NotNull String path) throws Exception {
+        try {
+            File file = new File(path);
 
-        File file = new File(path);
+            JAXBContext jaxbContext = JAXBContext.newInstance(ETTDescriptor.class);
+            Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+            ETTDescriptor d = (ETTDescriptor) jaxbUnmarshaller.unmarshal(file);
 
-        JAXBContext jaxbContext = JAXBContext.newInstance(ETTDescriptor.class);
-        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-        ETTDescriptor d = (ETTDescriptor) jaxbUnmarshaller.unmarshal(file);
-
-        return new Descriptor(d);
+            return new Descriptor(d);
+        }catch (ValidationException e){
+            throw  e;
+        }
+        catch (EmptyCollectionException e){
+            throw e;
+        }
+        catch (Exception e){
+            throw new Exception("file not loaded",e);
+        }
     }
 }
