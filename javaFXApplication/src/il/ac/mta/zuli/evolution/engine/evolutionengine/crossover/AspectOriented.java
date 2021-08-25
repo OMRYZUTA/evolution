3a752feba95ebebc1b457e5342e0f160e5351af2
OMRYZUTA <omrizu@mta.ac.il>
2021-08-25 16:50:33 +0300

    AspectOriented overview in comment

--- javaFXApplication/src/il/ac/mta/zuli/evolution/engine/evolutionengine/crossover/AspectOriented.java
+++ javaFXApplication/src/il/ac/mta/zuli/evolution/engine/evolutionengine/crossover/AspectOriented.java
@@ -17,7 +17,14 @@ public class AspectOriented<S extends Solution> extends Crossover<S> {
     @Override
     public List<S> crossover(List<S> selectedParents) {
         //TODO implement
+        //for each solution, in selected parents, organize as map of teacher per dh matrix  map<teacher, list <list<q>>>
+        //while there are more than 1 solution in general collection
+            //randomly select 2 parents
+            //crossover between 2 parents
+            //for each teacher, crossover between parent 1 and parent 2. so we have 2 new children per teacher
+            //combine to create 2 solutions (without conflicts)
+            //flatten both matrixes to return 2 solutions
 
-        return super.crossoverDH(selectedParents);
+        //return newGeneration
     }
 }
