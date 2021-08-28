cf11642ffdbb2aa3a3729f98553aab55e78fec34
Orna Lipkin <fifilp@gmail.com>
2021-08-28 10:51:25 +0300

    display raw quintets view

--- javaFXApplication/src/il/ac/mta/zuli/evolution/ui/solution/rawview/RawSolutionController.java
+++ javaFXApplication/src/il/ac/mta/zuli/evolution/ui/solution/rawview/RawSolutionController.java
@@ -2,8 +2,6 @@ package il.ac.mta.zuli.evolution.ui.solution.rawview;
 
 import il.ac.mta.zuli.evolution.dto.QuintetDTO;
 import il.ac.mta.zuli.evolution.dto.TimeTableSolutionDTO;
-import javafx.beans.binding.Bindings;
-import javafx.beans.property.SimpleDoubleProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
@@ -14,40 +12,30 @@ import java.util.stream.Collectors;
 public class RawSolutionController {
     @FXML
     private Label quintetListLabel;
-    @FXML
-    private Label scoreLabel;
 
-    private TimeTableSolutionDTO solution;
-    List<QuintetDTO> sortedQuintets;
-    private final SimpleDoubleProperty scoreProperty;
-    //TODO continue from here
     private final SimpleStringProperty rawQuintets; //private ObservableList<QuintetDTO> rawQuintets;?
 
     public RawSolutionController() {
-        scoreProperty = new SimpleDoubleProperty(0);
         rawQuintets = new SimpleStringProperty("");
     }
 
     public void setSolution(TimeTableSolutionDTO solution) {
-        this.solution = solution;
-        this.scoreProperty.set(solution.getTotalFitnessScore());
         //sorting quintets by day->hour->class->teacher
-        this.sortedQuintets = this.solution.getSolutionQuintets().stream()
+        List<QuintetDTO> sortedQuintets = solution.getSolutionQuintets().stream()
                 .sorted(QuintetDTO.getRawComparator())
                 .collect(Collectors.toList());
+        displaySolution(sortedQuintets);
     }
 
     @FXML
     private void initialize() {
-        scoreLabel.textProperty().bind(Bindings.format("Solution Score: %f", scoreProperty));
         quintetListLabel.textProperty().bind(rawQuintets);
     }
 
-    private void displaySolution() {
-        //where do we call displaySolution
+    private void displaySolution(List<QuintetDTO> quintets) {
         StringBuilder sb = new StringBuilder();
 
-        for (QuintetDTO q : sortedQuintets) {
+        for (QuintetDTO q : quintets) {
             sb.append(String.format("<%d, %d, %d, %d, %d>%n",
                     q.getDay().getValue(),
                     q.getHour(),
