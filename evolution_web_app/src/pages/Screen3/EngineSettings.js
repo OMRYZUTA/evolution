9cce66a38a3132fbc762d2fba5634a9c9f4d44ef
OMRYZUTA <omrizu@mta.ac.il>
2021-09-26 13:20:16 +0300

    change in schema, inserted population size and stride into engine settings

---
+++ evolution_web_app/src/pages/Screen3/EngineSettings.js
@@ -0,0 +1,62 @@
+
+const EngineSettings = ()=>{
+    return(
+        <Paper>
+            <TextField
+                required
+                id="outlined-required"
+                label="Population size"
+                defaultValue="100"
+            />
+            <TextField
+                required
+                id="outlined-required"
+                label="stride"
+                defaultValue="100"
+            />
+            <TextField
+                required
+                id="outlined-required"
+                label="Population size"
+                defaultValue="100"
+            />
+            <TextField
+                required
+                id="outlined-required"
+                label="Population size"
+                defaultValue="100"
+            /><TextField
+            required
+            id="outlined-required"
+            label="Population size"
+            defaultValue="100"
+        /><TextField
+            required
+            id="outlined-required"
+            label="Population size"
+            defaultValue="100"
+        /><TextField
+            required
+            id="outlined-required"
+            label="Population size"
+            defaultValue="100"
+        /><TextField
+            required
+            id="outlined-required"
+            label="Population size"
+            defaultValue="100"
+        /><TextField
+            required
+            id="outlined-required"
+            label="Population size"
+            defaultValue="100"
+        />
+            <TextField
+                required
+                id="outlined-required"
+                label="Population size"
+                defaultValue="100"
+            />
+        </Paper>
+    )
+}
