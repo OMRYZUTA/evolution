ad6c097af037518068cca45798a7cdc3db04f4d0
OMRYZUTA <omrizu@mta.ac.il>
2021-09-30 18:48:48 +0300

    timetable detials showing the relevant info

--- evolution_web_app/src/pages/Screen3/TimetableDetails.js
+++ evolution_web_app/src/pages/Screen3/TimetableDetails.js
@@ -19,6 +19,15 @@ const useStyles = makeStyles((theme) => ({
         justifyContent: "space-between",
         backgroundColor: "#D3D3D3", //light gray
     },
+    requirements: {
+        padding: 10,
+    },
+    schoolClass: {
+        border: 1,
+        borderBlockColor: "black",
+        padding: 10,
+        margin: 50,
+    }
 }));
 
 const TimetableDetails = ({timetable}) => {
@@ -70,16 +79,21 @@ const TimetableDetails = ({timetable}) => {
                 </AccordionSummary>
                 <AccordionDetails>
                     <Grid container direction={"column"} className={classes.root}>
-                        {timetable.rules.map(rule=>{
-                            return(
+                        <Typography>
+                            Hard rules Weight:
+                            {timetable.hardRulesWeight}
+                        </Typography>
+                        {timetable.rules.map(rule => {
+                            return (
                                 <Grid item>
                                     <Grid container className={classes.settings}>
-                                    <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                                        Rule Name: {rule.ruleName}
-                                    </Typography>
-                                        {rule.ruleName==="Sequentiality"&& <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                        <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                            Rule Name: {rule.ruleName}
+                                        </Typography>
+                                        {rule.ruleName === "Sequentiality" &&
+                                        <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
                                             Total Hours: {rule.totalHours}
-                                        </Typography> }
+                                        </Typography>}
                                         <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
                                             Rule Type: {rule.ruleType}
                                         </Typography>
@@ -90,10 +104,121 @@ const TimetableDetails = ({timetable}) => {
                     </Grid>
                 </AccordionDetails>
             </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>School classes </Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container direction={"column"} className={classes.root}>
+                        {Object.keys(timetable.schoolClasses).map((key, index) => {
+                            return (
+                                <Grid item>
+                                    <Grid container direction={"column"} sx={{border: 1}}>
+                                        <Grid item>
+                                            <Grid container className={classes.settings}>
+                                                <Typography>ID: {key} Name
+                                                    : {timetable.schoolClasses[key].name}</Typography>
 
+                                            </Grid>
+                                        </Grid>
+                                        <Grid item>
+                                            <Grid container className={classes.settings}>
+                                                <Typography>
+                                                    Requirements:
+                                                </Typography>
+                                                {timetable.schoolClasses[key].requirements.map(requirement => {
+                                                    return (
+                                                        <Grid container direction={"row"}
+                                                              className={classes.requirements}>
+                                                            <Grid item>
+                                                                <Typography>
+                                                                    Subject: {requirement.subject.name} Hours: {requirement.hours}
+                                                                </Typography>
+                                                            </Grid>
+                                                        </Grid>
+                                                    )
+                                                })
+                                                }
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
 
-            {/*{timetable.subjects.map(subject => <Typography>{subject.id} {subject.name}</Typography>)}*/}
+                                </Grid>
 
+                            )
+                        })}
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Subjects</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container className={classes.root} direction={"column"}>
+                        {Object.keys(timetable.subjects).map((key, index) => {
+                            return (
+                                <Grid item>
+                                    <Grid container className={classes.settings} direction={"column"}>
+                                        <Typography>
+                                            {timetable.subjects[key].id}. {timetable.subjects[key].name}
+                                        </Typography>
+                                    </Grid>
+                                </Grid>
+                            )
+                        })
+                        }
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Teachers</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container className={classes.root} direction={"column"}>
+                        {Object.keys(timetable.teachers).map((key, index) => {
+                            return (
+                                <Grid item>
+                                    <Grid container className={classes.settings} direction={"column"} sx={{border: 1}}>
+                                        <Typography>
+                                            {timetable.teachers[key].id}. {timetable.teachers[key].name}
+                                        </Typography>
+                                        <Typography>
+                                            working hours:{timetable.teachers[key].workingHours}
+                                        </Typography>
+                                        <Typography>
+                                            Teaches:
+                                        </Typography>
+                                        {console.log()}
+                                        {Object.keys(timetable.teachers[key].subjects).map((subjectKey, index) => {
+                                                return (
+                                                    <Typography>
+                                                        {timetable.teachers[key].subjects[subjectKey].id}. {timetable.teachers[key].subjects[subjectKey].name}
+                                                    </Typography>)
+                                            }
+                                        )
+                                        }
+                                    </Grid>
+                                </Grid>
+                            )
+                        })
+                        }
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
         </Paper>
     )
         ;
