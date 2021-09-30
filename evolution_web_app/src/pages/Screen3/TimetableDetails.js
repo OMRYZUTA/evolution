47cbc8e7d412208888d16cf0287e4469fd09ad7a
OMRYZUTA <omrizu@mta.ac.il>
2021-09-30 10:56:40 +0300

    general details are working

--- evolution_web_app/src/pages/Screen3/TimetableDetails.js
+++ evolution_web_app/src/pages/Screen3/TimetableDetails.js
@@ -11,13 +11,10 @@ import {TextField} from "@mui/material";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        padding: '50px 70px',
         spacing: 2,
-        justifyContent: 'flex-start',
-        alignItems: 'top-center',
+        justifyContent: "space-between"
     },
     settings: {
-        width: '100%',
         backgroundColor: "#D3D3D3", //light gray
     },
 }));
@@ -37,15 +34,54 @@ const TimetableDetails = ({timetable}) => {
                     <Typography>General Details</Typography>
                 </AccordionSummary>
                 <AccordionDetails>
-                    <Grid container className={classes.root}>
-
-                        <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
-                            Days: {timetable.days} Hours {timetable.hours}
-                        </Typography>
+                    <Grid container className={classes.root} direction={"column"}>
+                        <Grid item>
+                            <Grid container className={classes.root}>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    Days: {timetable.days}
+                                </Typography>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    Hours: {timetable.hours}
+                                </Typography>
+                            </Grid>
+                        </Grid>
+                        <Grid item>
+                            <Grid container className={classes.root}>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    Table ID: {timetable.ID}
+                                </Typography>
+                                <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                    Uploaded by: {timetable.uploadedBy}
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </Grid>
                 </AccordionDetails>
             </Accordion>
+            <Accordion>
+                <AccordionSummary
+                    expandIcon={<ExpandMoreIcon/>}
+                    aria-controls="panel1a-content"
+                    id="panel1a-header"
+                >
+                    <Typography>Rules</Typography>
+                </AccordionSummary>
+                <AccordionDetails>
+                    <Grid container direction={"column"}>
+                        {timetable.rules.map(rule=>{
+                            console.log(rule);
 
+                            return(
+                                <Grid item>
+                                    <Typography sx={{fontSize: 14}} color="text.secondary" gutterBottom>
+                                        Days: {} Hours {timetable.hours}
+                                    </Typography>
+                                </Grid>
+                            )
+                        })}
+                    </Grid>
+                </AccordionDetails>
+            </Accordion>
 
 
             {/*{timetable.subjects.map(subject => <Typography>{subject.id} {subject.name}</Typography>)}*/}
