13a157d84e29387ee9e4110ab53f45416c32f0ee
Orna Lipkin <fifilp@gmail.com>
2021-10-08 11:59:55 +0300

    remove a bunch of old folders fixed server.imll added GSON dependencies into server folder

---
+++ evolution_web_app/build/precache-manifest.752fd0bb60b09030ff051dc2f531b08a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+    {
+        "revision": "db1cf1b3a7d9ef756320",
+        "url": "/server_Web_exploded/static/css/main.dfdee277.chunk.css"
+    },
+    {
+        "revision": "db1cf1b3a7d9ef756320",
+        "url": "/server_Web_exploded/static/js/main.c318c69d.chunk.js"
+    },
+    {
+        "revision": "60265c63294eb36bc334",
+        "url": "/server_Web_exploded/static/js/runtime~main.eab1f59d.js"
+    },
+    {
+        "revision": "aeb5a3f80298f06d5d29",
+        "url": "/server_Web_exploded/static/js/2.13777e30.chunk.js"
+    },
+    {
+        "revision": "8ad3ae2b1eb9468f0e39ef0d5468211c",
+        "url": "/server_Web_exploded/index.html"
+    }
+];
--- ui-console/src/il/ac/mta/zuli/evolution/ui/QuintetFormatter.java
+++
@@ -1,7 +0,0 @@
-package il.ac.mta.zuli.evolution.ui;
-
-import il.ac.mta.zuli.evolution.dto.QuintetDTO;
-
-public interface QuintetFormatter {
-    String format(QuintetDTO q);
-}
