c9647655d71f5fb041c3dd892433e12d1b837f19
OMRYZUTA <omrizu@mta.ac.il>
2021-10-09 12:18:26 +0300

    redundent @

--- evolution_web_app/src/pages/Screen3/StrideGraph.js
+++ evolution_web_app/src/pages/Screen3/StrideGraph.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import Paper from '@@mui/material/Paper';
+import Paper from '@mui/material/Paper';
 import {makeStyles} from '@mui/styles'
 import {
     ArgumentAxis,
