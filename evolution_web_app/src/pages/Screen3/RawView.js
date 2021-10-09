b825d20465d09eea4e808c83c69164521e6fb11a
Orna Lipkin <fifilp@gmail.com>
2021-10-09 09:09:27 +0300

    sorted rawView

--- evolution_web_app/src/pages/Screen3/RawView.js
+++ evolution_web_app/src/pages/Screen3/RawView.js
@@ -2,14 +2,23 @@ import React from 'react';
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 
-const RawView= ({quintets})=>{
-    return(
-        <Grid container direction={"column"} >
+function getSortField(quintet) {
+    return `${quintet.day}-${quintet.hour}-${quintet.schoolClassID}-${quintet.teacherID}`;
+}
+
+const RawView = ({quintets}) => {
+    // Solution Raw view - sorted Day->Hour->class->teacher
+    const sortedQuintets = quintets.sort((q1, q2) => {
+        return q1.day - q2.day || q1.hour - q2.hour || q1.schoolClassID - q2.schoolClassID || q1.teacherID - q2.teacherID;
+    });
+
+    return (
+        <Grid container direction={"column"}>
             <Typography>
                 {`<D,H,C,T,S>`}
             </Typography>
-            {quintets.map(quintet=>{
-                return(
+            {sortedQuintets.map(quintet => {
+                return (
                     <Grid item>
                         <Typography>
                             {`<${quintet.day},${quintet.hour},${quintet.schoolClassID},${quintet.teacherID}>`}
--- evolution_web_app/src/pages/Screen3/TeacherView.js
+++ evolution_web_app/src/pages/Screen3/TeacherView.js
@@ -1,13 +1,5 @@
 import * as React from 'react';
-import {useCallback, useEffect, useState} from 'react';
-import Table from '@mui/material/Table';
-import TableBody from '@mui/material/TableBody';
-import TableCell from '@mui/material/TableCell';
-import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
-import TableRow from '@mui/material/TableRow';
-import Paper from '@mui/material/Paper';
-import Typography from "@mui/material/Typography";
+import {useCallback, useState} from 'react';
 import {makeStyles} from "@mui/styles"
 import Grid from "@mui/material/Grid";
 import DropDown from "../../components/Dropdown";
@@ -45,8 +37,6 @@ const TeacherView = ({quintets, days, hours, teachersObject}) => {
     const [teachers] = useState(extractTeachersFromTeacherObject(teachersObject));
     const [currentTeacher, setCurrentTeacher] = useState(teachers[0]);
     const [teacherQuintets, setTeacherQuintets] = useState(quintets.filter(quintet => quintet.teacherID === currentTeacher.id));
-
-
 
     const setValueInTeacher = useCallback((name, value) => {
         setTeacherQuintets(quintets.filter(quintet => quintet.teacherID === value));
